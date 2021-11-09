cc259ededdbd31de0df26f3f6376e144b6435f99
ayumitk <ayumee528@gmail.com>
2021-11-08 22:56:39 -0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,20 @@ const sass = require("gulp-sass")(require("sass"));
 const autoprefixer = require("gulp-autoprefixer");
 const changed = require("gulp-changed");
 const imagemin = require("gulp-imagemin");
-const imageminJpg = require("imagemin-jpeg-recompress");
-const imageminPng = require("imagemin-pngquant");
-const imageminGif = require("imagemin-gifsicle");
 const rename = require("gulp-rename");
-// const header = require("gulp-header");
-// const footer = require("gulp-footer");
 const ejs = require("gulp-ejs");
 const browserSync = require("browser-sync").create();
 
-const html = () =>
-  src(["src/ejs/**/*.ejs", "!" + "src/ejs/**/_*.ejs"])
+const html = () => {
+  return src(["src/ejs/**/*.ejs", "!" + "src/ejs/**/_*.ejs"])
     .pipe(ejs({}, {}, { ext: ".html" }))
     .pipe(rename({ extname: ".html" }))
     .pipe(dest("./docs"))
     .pipe(browserSync.stream());
+};
 
-const CSS = () =>
-  src("./src/sass/**/*.scss")
+const CSS = () => {
+  return src("./src/sass/**/*.scss")
     .pipe(
       sass({
         outputStyle: "compressed",
@@ -28,24 +24,22 @@ const CSS = () =>
     )
     .pipe(autoprefixer({ grid: true }))
     .pipe(dest("./docs/css"));
+};
 
-const image = () =>
-  src("./src/images/**/*.+(jpg|jpeg|png|gif)")
+const image = () => {
+  return src("./src/images/**/*.+(jpg|png|gif)")
     .pipe(changed("./docs/images"))
     .pipe(
       imagemin([
-        imageminPng(),
-        imageminJpg(),
-        imageminGif({
-          interlaced: false,
-          optimizationLevel: 3,
-          colors: 180,
-        }),
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
       ])
     )
     .pipe(dest("./docs/images"));
+};
 
-const watchFiles = () =>
+const watchFiles = () => {
   browserSync.init({
     server: {
       baseDir: "./docs",
@@ -53,10 +47,11 @@ const watchFiles = () =>
     open: "external",
     host: "192.168.0.24",
   });
-watch("./src/sass/**/*.scss", CSS);
-watch("./src/images/**/*.+(jpg|jpeg|png|gif)", image);
-watch("./src/ejs/**/*.ejs", html);
-watch("./docs/**/*").on("change", browserSync.reload);
+  watch("./src/sass/**/*.scss", CSS);
+  watch("./src/images/**/*.+(jpg|png|gif)", image);
+  watch("./src/ejs/**/*.ejs", html);
+  watch("./docs/**/*").on("change", browserSync.reload);
+};
 
 exports.html = html;
 exports.CSS = CSS;
